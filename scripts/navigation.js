5475e5808812e90c0520eb07d69326cd17ef2573
jorsi <jonathon.orsi@gmail.com>
2020-02-20 16:13:38 -0500

    remove meta refresh tag

--- scripts/navigation.js
+++
@@ -1,7 +0,0 @@
-const navigationEl = document.querySelector('#navigation');
-const hamburgerEl = document.querySelector('#navigation-hamburger');
-if (hamburgerEl) {
-    hamburgerEl.addEventListener('click', () => {
-        navigationEl.classList.toggle('is-menu-open');
-    });
-}
